35e50bfa76c8ed79b0191e840688e78e7c0612e5
Abdale Hassan <cabdale989@gmail.com>
2021-06-04 18:51:50 -0500

    connect node to MySQL

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
-const consoleTable = require('console.table');
+const cTable = require('console.table');
 const asciiart = require('asciiart-logo');
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'password',
+    database: 'employee_tracker'
+})
+
+
+
+connection.connect((err, res) => {
+    if(err) throw err;s
+})
