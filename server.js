50045fd876f129f7f24dd14da2d4f2d41f18f7da
Abdale Hassan <cabdale989@gmail.com>
2021-06-09 18:30:52 -0500

    add inquirer prompt to ask the user what they want and call a specific function as result

--- server.js
+++ server.js
@@ -12,9 +12,66 @@ const connection = mysql.createConnection({
     database: 'employee_tracker'
 })
 
+const startQuestions = () => {
+    inquirer
+        .prompt({
+            name: 'action',
+            type: 'rawlist',
+            message: 'What would you like to do?',
+            choices: [
+                'Add Employee',
+                'Add Role',
+                'Add Department',
+                'View All Employees',
+                'View All Roles',
+                'View All Departments',
+                'Update Employee Role',
+                'Quit',
+            ],
+        })
+        .then((answer) => {
+            // console.log('here  answer ', answer);
+            // console.log('here  answer.action ', answer.action);
+            switch (answer.action) {
+                case 'Add Employee':
+                    addEmployee();
+                    break;
+                case 'Add Role':
+                    addRole();
+                    break;
 
+                case 'Add Department':
+                    addDepartment();
+                    break;
+
+                case 'View All Employees':
+                    viewEmployees();
+                    break;
+
+                case 'View All Roles':
+                    viewRoles();
+                    break;
+
+                case 'View All Departments':
+                     viewAllDepartments();
+                    break;
+
+                case 'Update Employee Role':
+                    updateEmployeeRole();
+                    break;
+
+                    case 'Quit':
+                     console.log('Goodbye!!');
+                     break;
+
+                default:
+                    console.log(`Invalid action: ${answer.action}`);
+                    break;
+            }
+        });
+}
 
 connection.connect((err, res) => {
     if (err) throw err;
-
+    startQuestions();
 })
